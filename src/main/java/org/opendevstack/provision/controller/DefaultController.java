e7dced16f7ee933ba5a225df3eb7954c89efde1a
Tobias Unger <tobias.unger@opitz-consulting.com>
2019-04-25 16:52:24 +0200

    Delete unnecessary imports

--- src/main/java/org/opendevstack/provision/controller/DefaultController.java
+++ src/main/java/org/opendevstack/provision/controller/DefaultController.java
@@ -27,8 +27,6 @@ import org.opendevstack.provision.services.ConfluenceAdapter;
 import org.opendevstack.provision.services.JiraAdapter;
 import org.opendevstack.provision.services.RundeckAdapter;
 import org.opendevstack.provision.services.StorageAdapter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
--- src/test/java/org/opendevstack/provision/services/MailAdapterTest.java
+++ src/test/java/org/opendevstack/provision/services/MailAdapterTest.java
@@ -23,7 +23,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
