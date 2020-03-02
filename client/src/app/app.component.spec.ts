80d098e74785ce8e4b3c21d67fc1240fbcf8e79c
Jan Deppisch <jan.deppisch@boehringer-ingelheim.com>
2020-03-02 16:14:22 +0100

    PR feedback

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('Prov-App');
   });
 
 });
