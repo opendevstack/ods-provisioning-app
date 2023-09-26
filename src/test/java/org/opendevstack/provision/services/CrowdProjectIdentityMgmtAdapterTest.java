47f5774f395e6ab3dbaf92877fdde77b13a9eb43
Rodriguez,Hector (IT EDP) <hector_2.rodriguez@boehringer-ingelheim.com>
2023-09-26 12:03:30 +0200

    Fixed spotlessJavaCheck errors

--- src/test/java/org/opendevstack/provision/services/CrowdProjectIdentityMgmtAdapterTest.java
+++ src/test/java/org/opendevstack/provision/services/CrowdProjectIdentityMgmtAdapterTest.java
@@ -14,6 +14,10 @@
 
 package org.opendevstack.provision.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,121 +27,115 @@ import org.opendevstack.provision.adapter.IODSAuthnzAdapter;
 import org.opendevstack.provision.adapter.exception.IdMgmtException;
 import org.opendevstack.provision.model.OpenProjectData;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 public class CrowdProjectIdentityMgmtAdapterTest {
 
-    @InjectMocks
-    private CrowdProjectIdentityMgmtAdapter idMgr;
+  @InjectMocks private CrowdProjectIdentityMgmtAdapter idMgr;
 
-    @Mock
-    private IODSAuthnzAdapter manager;
+  @Mock private IODSAuthnzAdapter manager;
 
-    @Test
-    public void testGroupExists() {
-        String group = "xxx";
+  @Test
+  public void testGroupExists() {
+    String group = "xxx";
 
-        when(manager.existsGroupWithName(eq(group))).thenReturn(true);
-        assertTrue(idMgr.groupExists(group));
+    when(manager.existsGroupWithName(eq(group))).thenReturn(true);
+    assertTrue(idMgr.groupExists(group));
 
-        when(manager.existsGroupWithName(eq(group))).thenReturn(false);
-        assertFalse(idMgr.groupExists(group));
+    when(manager.existsGroupWithName(eq(group))).thenReturn(false);
+    assertFalse(idMgr.groupExists(group));
+  }
+
+  @Test
+  public void testUserExists() {
+    String principal = "user";
+
+    when(manager.existPrincipalWithName(principal)).thenReturn(true);
+    assertTrue(idMgr.userExists(principal));
+
+    when(manager.existPrincipalWithName(principal)).thenReturn(false);
+    assertFalse(idMgr.userExists(principal));
+  }
+
+  @Test
+  public void testCreateGroup() throws Exception {
+    String group = "xxx";
+
+    when(manager.addGroup(group)).thenReturn(group);
+    String groupInternal = idMgr.createGroupInternal(group);
+    assertEquals(group, groupInternal);
+
+    assertEquals(group, idMgr.createAdminGroup(group));
+    assertEquals(group, idMgr.createUserGroup(group));
+    assertEquals(group, idMgr.createReadonlyGroup(group));
+  }
+
+  @Test
+  public void testCreateNullGroup() {
+    assertThrows(IdMgmtException.class, () -> idMgr.createGroupInternal(null));
+  }
+
+  @Test
+  public void testCreateGroupSOAPErr() {
+    assertThrows(
+        IdMgmtException.class,
+        () -> {
+          String group = "xxx";
+          when(manager.addGroup(group)).thenThrow(IdMgmtException.class);
+          idMgr.createGroupInternal(group);
+        });
+  }
+
+  @Test
+  public void testValidateProject() throws Exception {
+    String principal = mockPrincipalExists("user", true);
+    String group = mockGroupExists("xxx", true);
+
+    OpenProjectData data = new OpenProjectData();
+    data.setProjectAdminGroup(group);
+    data.setProjectUserGroup(group);
+    data.setProjectReadonlyGroup(group);
+    data.setProjectAdminUser(principal);
+
+    idMgr.validateIdSettingsOfProject(data);
+
+    data.setProjectUserGroup("doesNotExistUG");
+    data.setProjectAdminGroup("doesNotExistAD");
+    data.setProjectReadonlyGroup("doesNotExistRO");
+
+    mockGroupExists(data.getProjectUserGroup(), false);
+    mockGroupExists(data.getProjectAdminGroup(), false);
+    mockGroupExists(data.getProjectReadonlyGroup(), false);
+
+    Exception testE = null;
+    try {
+      idMgr.validateIdSettingsOfProject(data);
+    } catch (IdMgmtException idEx) {
+      testE = idEx;
     }
+    assertNotNull(testE);
+    assertTrue(testE.getMessage().contains(data.getProjectUserGroup()));
+    assertTrue(testE.getMessage().contains(data.getProjectAdminGroup()));
+    assertTrue(testE.getMessage().contains(data.getProjectReadonlyGroup()));
 
-    @Test
-    public void testUserExists() {
-        String principal = "user";
-
-        when(manager.existPrincipalWithName(principal)).thenReturn(true);
-        assertTrue(idMgr.userExists(principal));
-
-        when(manager.existPrincipalWithName(principal)).thenReturn(false);
-        assertFalse(idMgr.userExists(principal));
+    mockPrincipalExists(principal, false);
+    testE = null;
+    try {
+      idMgr.validateIdSettingsOfProject(data);
+    } catch (IdMgmtException idEx) {
+      testE = idEx;
     }
+    assertNotNull(testE);
+    assertTrue(testE.getMessage().contains(data.getProjectUserGroup()));
+    assertTrue(testE.getMessage().contains(data.getProjectAdminUser()));
+  }
 
-    @Test
-    public void testCreateGroup() throws Exception {
-        String group = "xxx";
+  public String mockGroupExists(String groupName, boolean existsGroup) {
+    when(manager.existsGroupWithName(groupName)).thenReturn(existsGroup);
+    return groupName;
+  }
 
-        when(manager.addGroup(group)).thenReturn(group);
-        String groupInternal = idMgr.createGroupInternal(group);
-        assertEquals(group, groupInternal);
-
-        assertEquals(group, idMgr.createAdminGroup(group));
-        assertEquals(group, idMgr.createUserGroup(group));
-        assertEquals(group, idMgr.createReadonlyGroup(group));
-    }
-
-    @Test
-    public void testCreateNullGroup() {
-        assertThrows(IdMgmtException.class, () -> idMgr.createGroupInternal(null));
-    }
-
-    @Test
-    public void testCreateGroupSOAPErr() {
-        assertThrows(
-                IdMgmtException.class,
-                () -> {
-                    String group = "xxx";
-                    when(manager.addGroup(group)).thenThrow(IdMgmtException.class);
-                    idMgr.createGroupInternal(group);
-                });
-    }
-
-    @Test
-    public void testValidateProject() throws Exception {
-        String principal = mockPrincipalExists("user", true);
-        String group = mockGroupExists("xxx", true);
-
-        OpenProjectData data = new OpenProjectData();
-        data.setProjectAdminGroup(group);
-        data.setProjectUserGroup(group);
-        data.setProjectReadonlyGroup(group);
-        data.setProjectAdminUser(principal);
-
-        idMgr.validateIdSettingsOfProject(data);
-
-        data.setProjectUserGroup("doesNotExistUG");
-        data.setProjectAdminGroup("doesNotExistAD");
-        data.setProjectReadonlyGroup("doesNotExistRO");
-
-        mockGroupExists(data.getProjectUserGroup(), false);
-        mockGroupExists(data.getProjectAdminGroup(), false);
-        mockGroupExists(data.getProjectReadonlyGroup(), false);
-
-        Exception testE = null;
-        try {
-            idMgr.validateIdSettingsOfProject(data);
-        } catch (IdMgmtException idEx) {
-            testE = idEx;
-        }
-        assertNotNull(testE);
-        assertTrue(testE.getMessage().contains(data.getProjectUserGroup()));
-        assertTrue(testE.getMessage().contains(data.getProjectAdminGroup()));
-        assertTrue(testE.getMessage().contains(data.getProjectReadonlyGroup()));
-
-        mockPrincipalExists(principal, false);
-        testE = null;
-        try {
-            idMgr.validateIdSettingsOfProject(data);
-        } catch (IdMgmtException idEx) {
-            testE = idEx;
-        }
-        assertNotNull(testE);
-        assertTrue(testE.getMessage().contains(data.getProjectUserGroup()));
-        assertTrue(testE.getMessage().contains(data.getProjectAdminUser()));
-    }
-
-    public String mockGroupExists(String groupName, boolean existsGroup) {
-        when(manager.existsGroupWithName(groupName)).thenReturn(existsGroup);
-        return groupName;
-    }
-
-    public String mockPrincipalExists(String user, boolean exists) {
-        when(manager.existPrincipalWithName(user)).thenReturn(exists);
-        return user;
-    }
+  public String mockPrincipalExists(String user, boolean exists) {
+    when(manager.existPrincipalWithName(user)).thenReturn(exists);
+    return user;
+  }
 }
